1e7139a6251cdb38ee3d705bc5a1ece68cf8203a
LuckyLiski <luckyliski@MAIL.RU>
2013-08-06 22:15:59 +0400

    Create ds18b20.ino

---
+++ DS18B20/ds18b20.ino
@@ -0,0 +1,97 @@
+#include <OneWire.h>  //подключаем библиотеку
+ 
+OneWire  ds(10);  // Номер ноги к которой подключили датчик
+ 
+void setup(void) {
+  Serial.begin(9600);   // настройки ком порта
+}
+ 
+void loop(void) {
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+   
+  if ( !ds.search(addr)) {
+    Serial.println("Нет больше адресов.");
+    Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+   
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+ 
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC не является действительным!");
+      return;
+  }
+  Serial.println();
+  
+// Первый байт ROM указывает, какой чип
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20"); 
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Устройство не распознано");
+      return;
+  } 
+ 
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);      
+   delay(1000);     
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);        
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {          
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+ 
+//  Преобразование данных в фактическую температуру
+ 
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; 
+    if (data[7] == 0x10) {
+        raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    if (cfg == 0x00) raw = raw & ~7;  
+    else if (cfg == 0x20) raw = raw & ~3;
+    else if (cfg == 0x40) raw = raw & ~1; 
+ 
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print("  Temperature = ");
+  Serial.print(celsius);
+  Serial.print(" По Цельсию, ");
+  Serial.print(fahrenheit);
+  Serial.println("По Фаренгейту");
+}
