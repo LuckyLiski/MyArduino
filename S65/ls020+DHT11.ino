9b31de113d0638a4e2561ba96381689028569347
LuckyLiski <luckyliski@MAIL.RU>
2013-08-22 12:30:02 +0400

    Update ls020+DHT11.ino

--- S65/ls020+DHT11.ino
+++ S65/ls020+DHT11.ino
@@ -1,3 +1,18 @@
+/*
+ls020 pinout
+
+LED-          GND
+LED+          +9v
+1V8           n/c
+GND           GND
+2V9           +3,3v
+DAT   MOSI    pin 11
+CLK   SCK     pin 13
+CS            pin A4
+RESET         pin A3
+RS            pin 4
+*/
+
 #include <S65Display.h>
 S65Display lcd;
 
