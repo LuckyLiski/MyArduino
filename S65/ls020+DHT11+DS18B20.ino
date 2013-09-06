983ce915303874651d5de65b11e413f90558d13b
LuckyLiski <luckyliski@MAIL.RU>
2013-09-06 15:02:31 +0400

    Update ls020+DHT11+DS18B20.ino

--- S65/ls020+DHT11+DS18B20.ino
+++ S65/ls020+DHT11+DS18B20.ino
@@ -8,10 +8,18 @@ GND           GND
 2V9           +3,3v
 DAT   MOSI    pin 11
 CLK   SCK     pin 13
-CS            pin A4
+CS            pin A2
 RESET         pin A3
 RS            pin 4
 */
+
+/*
+Реле освещения на пин 7
+Кнопка освещения на пин 3 
+*/
+
+int light = 7; //Реле освещения на пин 7
+volatile int state = LOW;
 
 #include <S65Display.h>
 S65Display lcd;
@@ -37,32 +45,53 @@ DallasTemperature sensors(&oneWire);
 #include "DHT.h"
 #define DHTPIN 2     // what pin we're connected to 
 #define DHTTYPE DHT11   // DHT 11  
-DHT dht(DHTPIN, DHTTYPE); 
+DHT dht(DHTPIN, DHTTYPE);
 
+  long previousMillis = 0;
+  long interval = 1000; // Интервал обновления графика
+  int X=0;
+  int Y=85;
+  int XX=0;
+  int YY=85;
+  
 void setup()
 {
+// Кнопка на прерывании   
+  pinMode(light, OUTPUT);              // порт как выход
+  attachInterrupt(1, blink, RISING); // привязываем 1-е прерывание к функции blink(). 0 (на digital pin 2) и 1 (на digital pin 3)
+/*
+LOW — вызов прерывания всякий раз, когда на порту низкий уровень напряжения;
+CHANGE – прерывание вызывается при изменении значения на входе;
+RISING – вызов прерывания при изменении уровня напряжения с низкого (LOW) на высокое(HIGH)
+FALLING – вызов прерывания при изменении уровня напряжения с высокого (HIGH) на низкое (LOW)
+*/
+
   lcd.init(4); //spi-clk = Fcpu/4 //init LCD
-  lcd.clear(BGCOLOR); //clear screen
-  lcd.fillRect(0, 0, 175, 12, BLUE); //Верхний бар
-  lcd.drawText(0, 3, ("   MeteoStation v1.0"), 1, YELLOW, BLUE); //Верхний бар
-  lcd.fillRect(0, 120, 175, 131, BLUE); //Нижний бар
+  lcd.clear(BLACK); //clear screen
+  lcd.fillRect(0, 0, 175, 9, BLUE); //Верхний бар
+  lcd.fillRect(0, 122, 175, 131, BLUE); //Нижний бар
+  lcd.fillRect(1, 11, 174, 85, GREEN);
   
   sensors.begin();
+  
 }
 
-
+void blink()
+{
+  state = !state;                    // меняем значение на противоположное
+  digitalWrite(light, state);          // выводим state на реле 
+}
 
 void loop()
 {
+// Dallas  
   sensors.requestTemperatures(); // Send the command to get temperature
   float dallas = sensors.getTempCByIndex(0);
   char ds[7];   
   int dd = (dallas - (int)dallas) * 100; // множитель - число знаков после запятой. 10 - 1, 100 - 2, 1000 - 3 и т.д. 
   sprintf(ds,"%d.%d", (int)dallas, abs(dd));
-  lcd.drawText(10, 35, ("DS18B20:"), 1, RED, BGCOLOR);
-  lcd.drawText(100, 35, ds, 1, RED, BGCOLOR);
-  lcd.drawText(140, 35, ("\xF8\x43"), 1, RED, BGCOLOR);
-    
+
+// DHT 11    
   float h = dht.readHumidity();
   float t = dht.readTemperature();  
   
@@ -74,31 +103,51 @@ void loop()
   int tt = (t - (int)t) * 100; // множитель - число знаков после запятой. 10 - 1, 100 - 2, 1000 - 3 и т.д. 
   sprintf(temp,"%d.%d", (int)t, abs(tt)); 
   
+// Potentiometer  
   int armcurrent = analogRead(A0); //показания аналогово входа
   armcurrent = map(armcurrent, 0, 1023, 0, 99);
   char str[5]; // объявляем текстовую строку как массив "str" из 5-ти символов
   sprintf(str, " %02i", armcurrent);
+
+// Рисуем  
+  //lcd.drawText(0, 3, ("   MeteoStation v1.0"), 1, YELLOW, BLUE); //Верхний бар
+  lcd.drawText(5, 124, ("6/09/2013       11:53"), 1, YELLOW, BLUE); //Нижний бар
   
-  lcd.drawText(10, 111, "Poten:", 1, RED, BGCOLOR);
-  lcd.drawText(59, 111, str, 1, RED, BGCOLOR);
-  lcd.drawText(83, 111, "%", 1, RED, BGCOLOR);
+  //lcd.drawTextPGM(10, 50, PSTR("\x8F\xE0\xAE\xA2\xA5\xE0\xAA\xA0"), 1, RGB(0,255,0), RGB(255,0,0)); //Проверка
+    
+  lcd.drawText(5, 2, ("DS18B20:"), 1, YELLOW, BLUE);
+  lcd.drawText(100, 2, ds, 1, YELLOW, BLUE);
+  lcd.drawText(140, 2, ("\xF8\x43"), 1, YELLOW, BLUE);
+  
+  lcd.drawText(5, 90, ("Temperature:"), 1, RED, BLACK);
+  lcd.drawText(110, 90, temp, 1, RED, BLACK);
+  lcd.drawText(150, 90, ("\xF8\x43"), 1, RED, BLACK);
+  
+  lcd.drawText(5, 100, ("Humidity:"), 1, BLUE, BLACK);
+  lcd.drawText(110, 100, hum, 1, BLUE, BLACK);
+  lcd.drawText(150, 100, ("%"), 1, BLUE, BLACK);
+  //lcd.drawTextPGM(10,110, PSTR(" www.watterott.com  "), 1, RGB(  0,  0,255), RGB(255,  0,  0));
+ 
+  lcd.drawText(5, 110, "Potentiomet:", 1, WHITE, BLACK);
+  lcd.drawText(102, 110, str, 1, WHITE, BLACK);
+  lcd.drawText(150, 110, "%", 1, WHITE, BLACK);
   
   //lcd.drawLine(10, 10, 50, 40, RGB(0,255,255));
   //lcd.drawLine(10, 40, 50, 10, RGB(0,255,255));
   //lcd.drawCircle(88, 30, dia, RGB(255,255,0));
   //lcd.fillRect(120, 10, 150, 40, RGB(0,0,255));
   
-  lcd.drawText(10, 123, ("Menu     <-       ->"), 1, WHITE, BLUE); //Нижний бар
-  
-  //lcd.drawTextPGM(10, 50, PSTR("\x8F\xE0\xAE\xA2\xA5\xE0\xAA\xA0"), 1, RGB(0,255,0), RGB(255,0,0)); //Проверка
-  lcd.drawText(10, 15, ("Temp:"), 1, RED, BGCOLOR);
-  lcd.drawText(100, 15, temp, 1, RED, BGCOLOR);
-  lcd.drawText(140, 15, ("\xF8\x43"), 1, RED, BGCOLOR);
-  
-  lcd.drawText(10, 25, ("Hum:"), 1, BLUE, BGCOLOR);
-  lcd.drawText(100, 25, hum, 1, BLUE, BGCOLOR);
-  lcd.drawText(140, 25, ("%"), 1, BLUE, BGCOLOR);
-  //lcd.drawTextPGM(10,110, PSTR(" www.watterott.com  "), 1, RGB(  0,  0,255), RGB(255,  0,  0));
- 
-  //delay(500);
+
+// График температуры
+  if (millis() - previousMillis > interval) {
+  previousMillis = millis();
+  Y = map(dallas, 20, 60, 85, 11);
+  X++;
+  lcd.drawLine(X+1, 11, X+1, 85, RED);
+  lcd.drawLine(X, 11, X, 85, GREEN);
+  lcd.drawLine(XX, YY, X, Y, BLACK);
+  if (X >= 175) X = 0;
+  XX=X;
+  YY=Y;
+  }
 } 
